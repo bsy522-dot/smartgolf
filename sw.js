8ad0ed356f3fa0cfa4baa84b477b31e7b2660639
user <user@example.com>
2026-03-17 16:56:31 +0900

    SmartGolf v1.0 - 전국 골프장 파인더

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'smartgolf-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './courses_enriched.json',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
